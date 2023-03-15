2498dc569785f29569c0de875625468b58a94652
huangyu <598050554@qq.com>
2023-03-15 15:21:38 +0800

    add

--- modules/renderer/background.js
+++ modules/renderer/background.js
@@ -31,8 +31,6 @@ export function rendererBackground(context) {
 
     const baseMaps = {
       'mapboxStreets': mapboxStreets,
-      'mapboxOutdoors': mapboxOutdoors,
-      'mapboxDark': mapboxDark,
       'mapboxSatellite': mapboxSatellite,
       'mapboxSatelliteStreets': mapboxSatelliteStreets,
       'OpenStreetMap': osm,
