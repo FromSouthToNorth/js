f32702663656177be665086d3fa0778a12c01733
FromSouthToNorth <598050554@qq.com>
2023-03-25 11:11:15 +0800

    修复移动地图后路径 hash background 会被去掉问题

--- modules/util/object.js
+++ modules/util/object.js
@@ -1,6 +1,6 @@
 export function utilObjectOmit(obj, omitKeys) {
   return Object.keys(obj).reduce((result, key) => {
-    if (omitKeys.includes(key)) {
+    if (!omitKeys.includes(key)) {
       result[key] = obj[key]; // keep
     }
     return result;
